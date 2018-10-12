88c248764d4a78113a44533c9640dfbe229c38c2
florimondmanca <florimond.manca@gmail.com>
2018-10-12 10:53:29 +0200

    Add avatar field to user admin

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from users.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """Customized user admin panel."""
+
+    fieldsets = UserAdmin.fieldsets
+
+    # Update the second fieldset (corresponding to personal information)
+    # to include the avatar field.
+    # See below for default fieldsets:
+    # https://github.com/django/django/blob/master/django/contrib/auth/admin.py#L41
+    fieldsets[1][1]['fields'] += ('avatar',)
+
