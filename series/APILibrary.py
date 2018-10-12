50f5e83b03625ddcb3bc5cfafb8120e83f1c1876
Cecile Gontier <cecile.g@circles.asia>
2018-10-12 11:19:51 +0200

    API call to show details

--- series/APILibrary.py
+++ series/APILibrary.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from .show import Show
+from datetime import datetime
 
 
 api_url = "https://api.themoviedb.org/3/"
@@ -12,7 +13,7 @@ small_size = "w154"
 big_size = "w300/"
 
 
-def search_show(title):
+def search_show(title: str):
     """
     search title in API
 
@@ -31,4 +32,33 @@ def search_show(title):
             shows.append(show)
     return shows
 
+
+def get_show_details(id: int):
+    """
+    Search Api for the details of a show
+
+    :param id: id of the show in the APi
+    :return:
+    """
+    endpoint = "tv/" + str(id)
+    url = api_url + endpoint + api_key + "&" + language
+    resp = requests.get(url)
+    print(resp.status_code)
+    if resp.status_code == 200:
+        content = json.loads(resp.content)
+        title = content['name']
+        small_logo = icon_url + small_size + content["poster_path"]
+        big_logo = icon_url + big_size + content["poster_path"]
+        directors = [director["name"] for director in content["created_by"]]
+        creation_date = datetime.strptime(content["first_air_date"], "%Y-%m-%d")
+        genres = [genre["name"] for genre in content["genres"]]
+        last_episode_date = datetime.strptime(content["last_air_date"], "%Y-%m-%d")
+        if content["next_episode_to_air"]:
+            next_episode_date = datetime.strptime(content["next_episode_to_air"]["air_date"], "%Y-%m-%d")
+        else:
+            next_episode_date = None
+        synopsis = content["overview"]
+        return Show(id, title=title, small_logo_path=small_logo, big_logo_path=big_logo,
+                    synopsis=synopsis, directors=directors, genres=genres, creation_date=creation_date,
+                    last_episode_date=last_episode_date, next_episode_date=next_episode_date)
 
--- series/show.py
+++ series/show.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import datetime
 
 
 @dataclass
@@ -11,3 +12,12 @@ class Show:
     id: int
     title: str
     small_logo_path: str
+    big_logo_path: str = None
+    synopsis: str = None
+    creation_date: datetime = None
+    directors: [str] = None
+    genres: [str] = None
+    next_episode_date: datetime = None
+    last_episode_date: datetime = None
+
+
