f41c9c1e2caa317ebca790bf53934b69eff090a1
florimondmanca <florimond.manca@gmail.com>
2018-10-16 14:06:46 +0200

    Add Show model

--- series/models.py
+++ series/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Show(models.Model):
+    """Represents a show retrievable through the TMDB API."""
+
+    id = models.PositiveIntegerField(
+        primary_key=True,
+        help_text="The show's ID on the TMDB API.",
+    )
+    title = models.CharField(
+        max_length=100, null=True,
+        help_text="The show's name on the TMDB API.",
+    )
+    description = models.TextField(
+        null=True,
+        help_text="The show's overview on the TMDB API.",
+    )
+
+    def __str__(self) -> str:
+        return f'Show<{self.id}: {self.title}>'
