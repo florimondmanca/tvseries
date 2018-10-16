346df428101ca33d4d0225b88fa063a923ada976
Benoit Laurès <benoit.laures@student.ecp.fr>
2018-10-16 14:24:59 +0200

    solved setting issues

--- uptv/settings.py
+++ uptv/settings.py
@@ -21,7 +21,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'series',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,8 +115,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# User auth
-AUTH_USER_MODEL = "series.User"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
