79176772c59c5f817241928c50400553d4311993
florimondmanca <florimond.manca@gmail.com>
2018-10-16 14:29:07 +0200

    add first_followed and followers fields on APIShow model, configure APIShow admin

--- series/admin.py
+++ series/admin.py
@@ -5,3 +5,7 @@ from .models import APIShow
 @admin.register(APIShow)
 class APIShowAdmin(admin.ModelAdmin):
     """Admin panel for API shows."""
+
+    list_display = ('id', 'title', 'first_followed',)
+    list_filter = ('first_followed',)
+
--- series/models.py
+++ series/models.py
@@ -1,10 +1,30 @@
+"""Series models."""
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
 
+User = get_user_model()
+
 
 class APIShow(models.Model):
-    """Represents a show retrievable through the TMDB API."""
+    """Represents a show retrievable through the TMDB API.
+
+    An show is followed by zero, one or more users.
+    A user can follow (i.e. mark as favorite) zero, one or more shows.
+
+    Usage
+    -----
+    Access a show's followers:
+    >>> show = APIShow.objects.first()
+    >>> show.followers.all()
+    <QuerySet []>
+
+    Access a user's favorite shows:
+    >>> user = User.objects.first()
+    >>> user.favorites.all()
+    <QuerySet []>
+    """
 
     id = models.PositiveIntegerField(
         primary_key=True,
@@ -18,6 +38,15 @@ class APIShow(models.Model):
         null=True,
         help_text="The show's overview on the TMDB API.",
     )
+    first_followed = models.DateTimeField(
+        auto_now_add=True,
+        help_text='When the show was first followed.',
+    )
+    followers = models.ManyToManyField(
+        to=User, related_name='favorites', blank=True,
+        help_text='Users that will receive alerts about this show.',
+    )
 
     def __str__(self) -> str:
-        return f'Show<{self.id}: {self.title}>'
+        """Represent a show by its API ID."""
+        return str(self.id)
