b3f2406a24120333f21070e3879e36a143daa1dd
florimondmanca <florimond.manca@gmail.com>
2018-10-16 15:49:24 +0200

    add login and logout views

--- uptv/urls.py
+++ uptv/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='search_series'), name='index'),
     path('', include('series.urls')),
-    path('', include('users.urls')),
+    path('accounts/', include('users.urls')),
 ]
