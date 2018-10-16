5fcd429094c919549caeac871d62c275232da7d1
Cecile Gontier <cecile.g@circles.asia>
2018-10-16 16:17:00 +0200

    feat: - created view for the search results. - redirected the search form to this view to view results of search.

--- series/urls.py
+++ series/urls.py
@@ -4,4 +4,5 @@ from series import views
 
 urlpatterns = [
     path('', views.SearchSeriesView.as_view(), name='search_series'),
+    path('search/<str:term>', views.SearchResultsView.as_view(), name='search_results')
 ]
--- series/views.py
+++ series/views.py
@@ -1,7 +1,11 @@
 """Views for series app."""
-
-from django.views.generic import FormView
+from django.urls import reverse
+from django.views.generic import FormView, View
 from series.forms import SearchSeriesForm
+from series.show import Show
+from series.APILibrary import search_show
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -11,4 +15,17 @@ class SearchSeriesView(FormView):
     """Search for series."""
     template_name = 'series/search_series.html'
     form_class = SearchSeriesForm
-    success_url = '/'
+    success_url = '/search'
+
+    def get_success_url(self):
+        return reverse('search_results', kwargs={'term': self.request.POST['search_term']})
+
+
+class SearchResultsView(View):
+    """Result page after a search by user"""
+    template = 'series/search_results.html'
+
+    def get(self, request, term: str):
+        print("request ={}".format(request))
+        shows = search_show(term)
+        return render(template_name='series/search_results.html', request=request, context={'shows_list': shows})
