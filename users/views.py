4e6935ceb61380104b02b7fee2e4e117dfb910b4
Benoit Laurès <benoit.laures@student.ecp.fr>
2018-10-18 16:09:29 +0200

    improved redirection url

--- users/views.py
+++ users/views.py
@@ -1,11 +1,9 @@
 from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
 
 from .forms import SignUpForm
 from .models import User
 from series.models import APIShow
-from django.http import HttpResponse, HttpResponseRedirect
 
 
 class SignUpView(CreateView):
@@ -15,31 +13,21 @@ class SignUpView(CreateView):
     template_name = 'users/signup.html'
     success_url = '/'
 
+
 class FavedSeriesView(LoginRequiredMixin, ListView):
     template_name = "users/followed_series.html"
     context_object_name = 'faved_series_list'
 
-    login_url = 'login/'
+    login_url = '/accounts/login/'
     redirect_field_name = ''
 
     def get_queryset(self):
-        # user = self.request.user
-        # if self.request.user.is_anonymous:#is_authenticated():
-        #     # return None
-        #     return redirect('/accounts/login')#None#HttpResponseRedirect('accounts/login')
-        # else:
-            return APIShow.objects.filter(followers=self.request.user)
+        return APIShow.objects.filter(followers=self.request.user)
 
-    # def get_queryset(self):
-    #     username = self.request.GET.get('username',None)
-    #     user = None
-    #     if username:
-    #         try:
-    #             user = User.objects.get(username=username)
-    #         except (User.DoesNotExist, User.MultipleObjectsReturned):
-    #             pass
-    #     if user:
-    #         return User.objects.filter(user=user)
-    #     return User.objects.none()
-
-
+    def get_redirect_url(self):
+        """Return the user-originating redirect URL if it's safe."""
+        redirect_to = self.request.POST.get(
+            self.redirect_field_name,
+            self.request.GET.get(self.redirect_field_name, '')
+        )
+        return redirect_to
