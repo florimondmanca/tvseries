82b712f2781b1ef1f881e87ea4bebe8b1765a8c5
Benoit Laurès <benoit.laures@student.ecp.fr>
2018-10-18 15:49:26 +0200

    changed url to access faved series, redirect logged out users

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 
 from .forms import SignUpForm
 from .models import User
 from series.models import APIShow
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 class SignUpView(CreateView):
@@ -13,18 +15,20 @@ class SignUpView(CreateView):
     template_name = 'users/signup.html'
     success_url = '/'
 
-
-class UserProfile(ListView):
+class FavedSeriesView(LoginRequiredMixin, ListView):
     template_name = "users/followed_series.html"
     context_object_name = 'faved_series_list'
-    # model = APIShow
+
+    login_url = 'login/'
+    redirect_field_name = ''
 
     def get_queryset(self):
-        user = self.request.user
-        if user.is_anonymous:
-            return None
-        else:
-            return user.favorites.all()
+        # user = self.request.user
+        # if self.request.user.is_anonymous:#is_authenticated():
+        #     # return None
+        #     return redirect('/accounts/login')#None#HttpResponseRedirect('accounts/login')
+        # else:
+            return APIShow.objects.filter(followers=self.request.user)
 
     # def get_queryset(self):
     #     username = self.request.GET.get('username',None)
