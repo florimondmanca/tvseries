90e7a90312a13eabef051ac05b753f28bcd7f967
florimondmanca <florimond.manca@gmail.com>
2018-10-23 14:36:38 +0200

    Fix timezone when getting airing shows

--- tmdb/client.py
+++ tmdb/client.py
@@ -12,6 +12,7 @@ from typing import List
 
 import requests
 
+from django.conf import settings
 from tmdb.pagination import collect_paginated_results
 from .parsers.shows import ShowParser
 from .datatypes import Show
@@ -109,7 +110,10 @@ class TMDBClient:
         """
         return collect_paginated_results(
             fetch_page=lambda page: self._request('tv/airing_today', {
-                'page': page
+                'page': page,
+                # Results differ by timezone, use the one configured
+                # on this server.
+                'timezone': settings.TIME_ZONE,
             }),
             total_pages=lambda data: data['total_pages'],
             extract=lambda data: [show['id'] for show in data['results']],
--- uptv/settings.py
+++ uptv/settings.py
@@ -142,7 +142,7 @@ LOGGING = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
