896835a71eea6188fd0b8990994009f7e1ae85b0
Benoit Laurès <benoit.laures@student.ecp.fr>
2018-10-26 10:53:38 +0200

    Added tests

--- alerts/notifications.py
+++ alerts/notifications.py
@@ -2,13 +2,15 @@ from django.core.mail import send_mail
 from users.models import User
 
 class NotificationBackend:
+    """Backend for generic notifications."""
     def notify(self, user:User):
         pass
 
 
 class EmailNotifier(NotificationBackend):
+    """Notifications with emails."""
     _subject = 'Your favourite series arrive today !'
-    _content = 'New episodes for these series arrive today : '
+    _basic_content = 'New episodes for these series arrive today : '
     _mail_from = 'new_series@uptv.com'
 
     def notify(self, user: User):
@@ -21,7 +23,12 @@ class EmailNotifier(NotificationBackend):
         )
 
     def get_full_content(self, user: User):
-        mail_content = self._content
+        """Create the full content of an email."""
+        mail_content = self._basic_content
         for followed_show in user.favorites.all():
+            # Add show's title to the content
             mail_content += followed_show.title + "\n"
         return mail_content
+
+    def get_subject(self):
+        return self._subject
---
+++ alerts/tests.py
@@ -0,0 +1,28 @@
+from django.core import mail
+from django.test import TestCase
+
+from users.models import User
+from series.models import APIShow
+from alerts.notifications import EmailNotifier
+
+class EmailTest(TestCase):
+    """Email notification testing."""
+    def test_send_email(self):
+        # Add user and tv show
+        user = User.objects.create_user(username='username', email='to@example.com', password='password')
+        user.save()
+        tv_show = APIShow(id='1', title='Test TV Show')
+        tv_show.save()
+        tv_show.followers.add(user)
+
+        notifier = EmailNotifier()
+        notifier.notify(user)
+
+        # Test that one message has been sent.
+        self.assertEqual(len(mail.outbox), 1)
+
+        # Verify that the subject of the first message is correct.
+        self.assertEqual(mail.outbox[0].subject, notifier.get_subject())
+
+        # Verify that the body of the first message is correct.
+        print(mail.outbox[0].body, 'New episodes for these series arrive today : Test TV Show')
