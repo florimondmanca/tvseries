a81254e37bce4cbaa130e4aa678aa76d9cb04d85
florimondmanca <florimond.manca@gmail.com>
2018-10-29 23:46:55 +0100

    Refactor subscription test cases

--- tests/test_series/test_subscribe.py
+++ tests/test_series/test_subscribe.py
@@ -10,6 +10,7 @@ User = get_user_model()
 
 
 class SubscriptionTest:
+    """Base test holder for subscription test cases."""
 
     class TestCase(TestCase):
 
@@ -23,25 +24,35 @@ class SubscriptionTest:
         def _force_login(self):
             self.client.force_login(self.user)
 
+        def call(self, pk: int = 1402):
+            raise NotImplementedError
+
+        def call_logged_in(self, should_be_subscribed: bool):
+            self._force_login()
+            response = self.call()
+            self.assertEqual(response.status_code, 200)
+            show = APIShow.objects.get(pk=self.show.pk)
+            self.assertEqual(
+                should_be_subscribed,
+                show.followers.filter(pk=self.user.pk).exists(),
+            )
+
+        # This test is common to both test cases
+        def test_if_not_authenticated_then_unauthorized_with_json_error(self):
+            response = self.call()
+            self.assertEqual(response.status_code, 401)
+            data = json.loads(response.content)
+            self.assertIn('error', data)
+
 
 class TestSubscribe(SubscriptionTest.TestCase):
     """Test subscribing to a show."""
 
-    def _subscribe(self, pk=1402):
+    def call(self, pk=1402):
         return self.client.post(f'/subscribe/{pk}')
 
-    def test_if_not_authenticated_then_unauthorized_with_json_error(self):
-        response = self._subscribe()
-        self.assertEqual(response.status_code, 401)
-        data = json.loads(response.content)
-        self.assertIn('error', data)
-
     def test_if_not_subscribed_then_subscribed_after_call(self):
-        self._force_login()
-        response = self._subscribe()
-        self.assertEqual(response.status_code, 200)
-        show = APIShow.objects.get(pk=self.show.pk)
-        self.assertTrue(show.followers.filter(pk=self.user.pk).exists())
+        self.call_logged_in(should_be_subscribed=True)
 
     def test_if_show_does_not_exist_then_it_is_fetched_from_api(self):
         # TODO with a mock of the TMDB API
@@ -51,23 +62,13 @@ class TestSubscribe(SubscriptionTest.TestCase):
 class TestUnsubscribe(SubscriptionTest.TestCase):
     """Test unsubscribing to a show."""
 
-    def _unsubscribe(self, pk=1402):
+    def call(self, pk=1402):
         return self.client.delete(f'/subscribe/{pk}')
 
-    def test_if_not_authenticated_then_unauthorized_with_json_error(self):
-        response = self._unsubscribe()
-        self.assertEqual(response.status_code, 401)
-        data = json.loads(response.content)
-        self.assertIn('error', data)
-
     def test_if_subscribed_then_unsubscribed_after_call(self):
-        self._force_login()
-        response = self._unsubscribe()
-        self.assertEqual(response.status_code, 200)
-        show = APIShow.objects.get(pk=self.show.pk)
-        self.assertFalse(show.followers.filter(pk=self.user.pk).exists())
+        self.call_logged_in(should_be_subscribed=False)
 
     def test_if_show_does_not_exist_then_no_error(self):
         self._force_login()
-        response = self._unsubscribe(pk=42)
+        response = self.call(pk=42)
         self.assertEqual(response.status_code, 200)
