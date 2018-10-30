900d1f1d91ba4d4ecad5585218a6154ed4c5a146
Benoit Laurès <benoit.laures@student.ecp.fr>
2018-10-30 15:33:28 +0100

    Added button to send emails

--- series/admin.py
+++ series/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import APIShow
+from alerts.jobs import AiringShowsJob
 
 
 @admin.register(APIShow)
@@ -9,3 +10,11 @@ class APIShowAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'title', 'first_followed',)
     list_filter = ('first_followed',)
+    actions = ['email_followers']
+
+    def email_followers(self, request, queryset):
+        notifier = AiringShowsJob()
+        for show in queryset:
+            notifier.notify_followers(show)
+
+    email_followers.short_description = "Email followers of selected shows"
