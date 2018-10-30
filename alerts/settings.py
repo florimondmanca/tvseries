2727b8c0caaf33729f3a9ea225da15f1d83d7ece
florimondmanca <florimond.manca@gmail.com>
2018-10-30 17:49:38 +0100

    move alerts tests file to the tests package

--- alerts/settings.py
+++ alerts/settings.py
@@ -7,4 +7,3 @@ from django.utils.dateparse import parse_time
 
 ACTIVE = getattr(settings, 'ALERTS_ACTIVE', False)
 RUN_TIME = parse_time(getattr(settings, 'ALERTS_RUN_TIME', '08:00:00'))
-print(RUN_TIME)
