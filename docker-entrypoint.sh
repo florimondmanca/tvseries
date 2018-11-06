fd1d6622a0dffb44c8d69cec87774125903135f0
florimondmanca <florimond.manca@gmail.com>
2018-11-06 12:01:59 +0100

    Update .gitignore, fix container unable to run migrations upon startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Wait for DB container to be up
+sleep 3s
 # Run migrations and then the production server
 python manage.py migrate --noinput
 exec gunicorn uptv.wsgi -c python:uptv.gunicorn
